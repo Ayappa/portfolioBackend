7f591f7266df5de41024d1d58e87015b5b675220
Ayappa <ayappaganesh1995@gmail.com>
2020-07-27 11:19:47 -0400

    k

--- server.js
+++ server.js
@@ -2,13 +2,6 @@ const express = require("express");
 const app = express();
 
 var cors = require("cors");
-app.use(cors());
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	next();
-});
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 app.use(express.json({ extended: false }));
 
